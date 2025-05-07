a9199a58c8ad6370e85f80adeab3ad4e061330b9
RazzeRRo <jaakkosaikkonen423@gmail.com>
2025-05-07 21:10:14 +0300

    Csharp korjaus

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const board = document.getElementById("game-board");
 const startButton = document.getElementById("start-button");
 
 const imageNames = [
-  "C.png", "CSharp.png", "C++.png", "go.png",
+  "C.png", "Csharp.png", "C++.png", "go.png",
   "html.png", "java.png", "JavaScript.png", "python.png"
 ];
 
